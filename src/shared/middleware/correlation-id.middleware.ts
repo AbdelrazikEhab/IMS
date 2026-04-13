19a16017110b0fae9f0daa09f0712e8bee3b8611
abdelrazikehab <abdelrazikehab7@gmail.com>
2026-04-13 16:26:40 +0200

    add feature

--- src/shared/middleware/correlation-id.middleware.ts
+++ src/shared/middleware/correlation-id.middleware.ts
@@ -1,6 +1,6 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import { randomUUID } from 'crypto';
 import { AppRequest } from '../types/request.type';
 
 @Injectable()
@@ -10,7 +10,7 @@ export class CorrelationIdMiddleware implements NestMiddleware {
     const requestId =
       (req.headers['x-request-id'] as string) ||
       (req.headers['x-correlation-id'] as string) ||
-      uuidv4();
+      randomUUID();
 
     appReq.requestId = requestId;
     res.setHeader('X-Request-Id', requestId);
